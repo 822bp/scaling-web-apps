d323a6df5bc855f737105a5e80124c65483ad01f
benjamin.pascale <benjamin.pascale@hoffmann-group.com>
2023-11-19 11:27:03 +0100

    adds flask server

---
+++ server/app.py
@@ -0,0 +1,49 @@
+from flask import Flask, json
+
+from flaskext.mysql import MySQL
+
+app = Flask(__name__)
+mysql = MySQL()
+app.config["MYSQL_DATABASE_USER"] = "root"
+app.config["MYSQL_DATABASE_PASSWORD"] = "root"
+app.config["MYSQL_DATABASE_DB"] = "store"
+app.config["MYSQL_DATABASE_HOST"] = "localhost"
+mysql.init_app(app)
+
+def makeQuery(query):
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute(query)
+    return cursor.fetchall()
+
+def makeJSON(data):
+    jsonData = []
+    for idobj, obj in enumerate(data):
+        currentObj = {}
+        for idx, x in enumerate(obj):
+            if(idx == 0):
+                currentObj["id"] = x
+            elif(idx == 1):
+                currentObj["product_name"] = x
+            elif(idx == 2):
+                currentObj["product_image"] = x
+            elif(idx == 3):
+                currentObj["order_amount"] = x
+            elif(idx == 4):
+                currentObj["order_price"] = x
+        jsonData.append(currentObj)
+    return jsonData
+
+
+@app.route("/products")
+def products():
+    data = makeJSON(makeQuery("SELECT * FROM products"))
+    response = app.response_class(
+        response=json.dumps(data),
+        status=200,
+        mimetype="application/json"
+    )
+    return response
+
+if (__name__ == "__main__"):
+    app.run(debug=True)
